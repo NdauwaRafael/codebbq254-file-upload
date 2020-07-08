685531f6620dc3b97e32077acff0f9323752680a
Raphael <raphaelndauwa@gmail.com>
2020-07-08 16:43:42 +0300

    initialize application

---
+++ src/main/java/com/example/codebbqfileupload/CodebbqFileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.example.codebbqfileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebbqFileUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodebbqFileUploadApplication.class, args);
+    }
+
+}
